9157e58d20e856b160a8d0e0fee5c33227c2ede6
Genrikh <lessergv@gmail.com>
2024-09-17 11:40:16 +0400

    before deploy hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react";
+import { Slider, SliderProps } from "@mui/material";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+  return (
+    <Slider
+      sx={{
+        // стили для слайдера // пишет студент
+        width: "147px",
+        height: "4px",
+        background: "white",
+        "& .MuiSlider-thumb": {
+          backgroundColor: "white", // Цвет ручки (thumb)
+          width: "18px", // Размер ручки
+          height: "18px",
+          border: "1px solid #01CB22",
+          "&::before": {
+            content: '""',
+            width: "6px", // Размер точки
+            height: "6px",
+            backgroundColor: "#01CB22", // Цвет точки
+            borderRadius: "50%", // Круглая форма точки
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)", // Центровка точки в ручке
+          },
+        },
+      }}
+      disableSwap
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  );
+};
 
-export default SuperRange
+export default SuperRange;
